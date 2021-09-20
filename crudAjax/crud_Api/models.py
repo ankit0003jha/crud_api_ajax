bce206a4a5120057781e4de2457cadde05ec2d8f
Ankit jha <65860426+ankit0003jha@users.noreply.github.com>
2021-09-20 10:25:09 +0530

    Update models.py

--- crudAjax/crud_Api/models.py
+++ crudAjax/crud_Api/models.py
@@ -7,8 +7,6 @@ class Employee(models.Model):
     name = models.CharField(max_length=70)
     email = models.EmailField(max_length=70)
     task = models.CharField(max_length=500)
-    designation = models.CharField(max_length=70)
-    joining_date = models.DateField(auto_now_add = True,blank=True, null=True)
     
 
     def __str__(self):
